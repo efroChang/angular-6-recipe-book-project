8bcaac14505090a7137814a9a7dccc5be9915dbc
Eric Chang <efrochang@gmail.com>
2018-05-17 18:18:54 -0500

    Add Ingredient array to Recipe model

--- src/app/recipes/recipe-detail/recipe.service.ts
+++ src/app/recipes/recipe-detail/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from "../recipe.model";
 import { Output, EventEmitter } from "@angular/core";
+import { Ingredient } from "../../shared/ingredient.model";
 
 
 export class RecipeService 
@@ -9,8 +10,25 @@ export class RecipeService
     // Create Recipe Array
     recipes: Recipe[] =
     [
-        new Recipe('Spegatte with Meatballs', 'This is a test recipe 1', 'https://images-gmi-pmc.edge-generalmills.com/80fd8638-9b0d-4cba-ba99-9c4b75b4a20c.jpg'),
-        new Recipe('The Heart Pizza', 'This is a test recipe 2', 'https://img.sndimg.com/food/image/upload/w_706,h_398,c_fill,fl_progressive,q_80/v1/img/recipes/70/16/5/VpYCAyQQw2c3LFvutQsZ_Final%201%20-%20Heart%20Shaped%20Pizza.jpg')
+        new Recipe(
+            'Spaghetti with Meatballs', 
+            'This is a test recipe 1', 
+            'https://images-gmi-pmc.edge-generalmills.com/80fd8638-9b0d-4cba-ba99-9c4b75b4a20c.jpg',
+            [
+                new Ingredient( 'Noodles', 2 ),
+                new Ingredient( 'Meatballs', 5 )
+            ]
+        ),
+        new Recipe(
+            'The Heart Pizza', 
+            'This is a test recipe 2', 
+            'https://img.sndimg.com/food/image/upload/w_706,h_398,c_fill,fl_progressive,q_80/v1/img/recipes/70/16/5/VpYCAyQQw2c3LFvutQsZ_Final%201%20-%20Heart%20Shaped%20Pizza.jpg',
+            [
+                new Ingredient( 'Bread', 5 ),
+                new Ingredient( 'Cheese', 10 ),
+                new Ingredient( 'Hame', 3 )
+            ]
+        )
     ];
 
     // To get a COPY of the Recipe array
