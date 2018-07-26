52b9ff2d6d81359fb26c4c5943d855574e0d4990
Eric Chang <efrochang@gmail.com>
2018-07-26 11:34:23 -0500

    Reactive Form: Unsubscribe recipeChanged event

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { Subscription } from '../../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
 
   // Create Recipe Array
   recipes: Recipe[];
+
+  subscription: Subscription;
 
   constructor( private recipeService: RecipeService,
                private router: Router,                      // [KEY]: to navigate
@@ -21,7 +24,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() 
   { 
     // [KEY]: To subscribe to the RecipeChanged event from RecipeService and update the local copy
-    this.recipeService.recipeChanged.subscribe(
+    this.subscription = this.recipeService.recipeChanged.subscribe(
       (updatedRecipes: Recipe[]) => {
         this.recipes = updatedRecipes;
       }
@@ -37,5 +40,9 @@ export class RecipeListComponent implements OnInit {
       ['new'], 
       { relativeTo: this.route } 
     );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
