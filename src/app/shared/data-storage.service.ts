de85861b6d0937a394fbaa05f414de5837578351
Eric Chang <efrochang@gmail.com>
2018-09-27 17:02:50 -0500

    HTTP: GET recipes from Firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Observable } from "rxjs";
-import { Recipe } from "../recipes/recipe.model";
 
 @Injectable()
 export class DataStorageService {
@@ -20,16 +19,8 @@ export class DataStorageService {
 
     fetchRecipes() {
         return this.http.get('https://efro-ng-recipe-book.firebaseio.com/recipes.json')
-            .map((response: Response) => {
-                let recipes: Array<Recipe> = response.json();
-                recipes.forEach( recipe => {
-                    if (!recipe['ingredients']) {
-                        recipe['ingredients'] = [];
-                    }
-                });
-                return recipes;
-            })
-            .subscribe((recipes: Array<Recipe>) => {
+            .subscribe((response: Response) => {
+                let recipes = response.json();
                 this.recipeService.setRecipes( recipes );
             });
     }
