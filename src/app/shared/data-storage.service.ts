e37f86728ba5ed9aa0fefe7ae9b06def5eadb268
Eric Chang <efrochang@gmail.com>
2018-09-27 15:23:54 -0500

    HTTP: PUT data into Firebase

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import { RecipeService } from "../recipes/recipe.service";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class DataStorageService {
+
+    constructor( private http: Http,
+                 private recipeService: RecipeService) {
+    }
+
+    storeRecipes(): Observable<Response> {
+        return this.http.put(
+            'https://efro-ng-recipe-book.firebaseio.com/recipes.json',
+            this.recipeService.getRecipes()
+        );
+    }
+}
