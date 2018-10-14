9d27572fac0ae0d3be02b2866622ff6b5788689f
Eric Chang <efrochang@gmail.com>
2018-10-14 14:25:00 -0500

    Auth: [KEY] Get, Store, and Use AWT token form Firebase

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import * as firebase from 'firebase';
 
 export class AuthService {
+    private token: string;
 
-    signupUser(email: string, password: string) {
+    public signupUser(email: string, password: string) {
 
         firebase.auth().createUserWithEmailAndPassword(email, password)
             .catch(
@@ -10,14 +11,28 @@ export class AuthService {
             );
     }
 
-    signinUser(email: string, password: string) {
+    public signinUser(email: string, password: string) {
 
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(
-                response => console.log(response)
+                response => {
+                    firebase.auth().currentUser.getIdToken()                // [KEY]: Get and store current AWT token right away.
+                        .then(
+                            (token: string) => this.token = token
+                        )
+                }
             )
             .catch(
                 error => console.log(error)
             );
+    }
+
+    public getToken(): string {
+        firebase.auth().currentUser.getIdToken()
+            .then(
+                (token: string) => this.token = token                       // [KEY]: Update the local token
+            )
+
+        return this.token;                                                  // [KEY]: But return the local copy w/o waiting for the update
     }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,23 +2,29 @@ import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Observable } from "rxjs";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable()
 export class DataStorageService {
 
     constructor( private http: Http,
-                 private recipeService: RecipeService) {
+                 private recipeService: RecipeService,
+                 private authService: AuthService) {
     }
 
     storeRecipes(): Observable<Response> {
+        const token: string = this.authService.getToken();
+
         return this.http.put(
-            'https://efro-ng-recipe-book.firebaseio.com/recipes.json',
+            'https://efro-ng-recipe-book.firebaseio.com/recipes.json?auth=' + token,
             this.recipeService.getRecipes()
         );
     }
 
     fetchRecipes() {
-        return this.http.get('https://efro-ng-recipe-book.firebaseio.com/recipes.json')
+        const token: string = this.authService.getToken();
+
+        return this.http.get('https://efro-ng-recipe-book.firebaseio.com/recipes.json?auth=' + token)
             .subscribe((response: Response) => {
                 let recipes = response.json();
                 this.recipeService.setRecipes( recipes );
